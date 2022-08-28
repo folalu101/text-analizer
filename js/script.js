b33d250d0a531b0ae9295cfd6bdaa005732cde2a
A.Adefolalu B <adefolalucode101@gmail.com>
2022-08-28 13:47:51 +0100

    add logic to check for empty string

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 //Business Logic
 function wordCounter(text) {
+    // to check for empty string
+    if (text.length === 0){
+        return 0;
+    }
     let wordCount = 0;
     const wordArray = text.split(" ");
     wordArray.forEach(function(word) {
